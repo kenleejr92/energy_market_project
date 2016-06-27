920120c66f2efa123bb683f4d3f18121dacc3ce6
kenleejr92 <kenleejr92@gmail.com>
2016-06-27 00:02:50 -0500

    LMP_spark_reduction refined

--- scripts/aws_emr_scripts/LMP_spark_reduction.py
+++ scripts/aws_emr_scripts/LMP_spark_reduction.py
@@ -1,24 +1,47 @@
 from pyspark import SparkConf, SparkContext
-from datetime import datetime
-
 
 
 conf = SparkConf().setMaster("local").setAppName("LMP_spark_reduction")
 sc = SparkContext(conf = conf)
 
-def format(line):
-    temp = line.split(",")
-    date = temp[0].split("/");
-    new_date = date[2] + "/" + date[1] + "/" + date[0];    
-    return [new_date + "," + temp[1] + "," + temp[2] + "," + temp[3]];
 
+#Load LMP data
+raw_LMP = sc.textFile("/home/kenlee/energy_market_project/LMP_data/DAM_Hr_LMP_2010/merge_test.csv")
 
-raw_LMP = sc.textFile("/home/kenlee/Energy_Market_Project/LMP_data/DAM_Hr_LMP_2010/merge_test.csv")
 #remove csv header
 header = raw_LMP.first()
 raw_LMP = raw_LMP.filter(lambda x: x!=header)
-formatted_LMP = raw_LMP.flatMap(format)
-formatted_LMP.saveAsTextFile("/home/kenlee/Energy_Market_Project/LMP_data/DAM_Hr_LMP_2010/formatted_merge_test");
+
+#create (datetime, (bus_name,price)) pair
+def create_pairs(line):
+    temp = line.split(",")
+    return (temp[0] + " " +  temp[1], [(temp[2],temp[3])])
+pairs = raw_LMP.map(create_pairs)
+
+#reduce by datetime key
+reduced_by_date = pairs.reduceByKey(lambda x,y: x + y)
+
+#sort by bus_name for each date
+sorted_by_bus_name = reduced_by_date.mapValues(lambda x: sorted(x, key=lambda y: y[0]))
+
+#get bus_names in order
+first_date = sorted_by_bus_name.take(1)
+bus_names = []
+for bus_price in first_date[0][1]:
+    bus_names.append(bus_price[0])
+#....save bus_names to file
+
+#delete bus_name
+no_bus_names = sorted_by_bus_name.mapValues(lambda x: [i[1] for i in x])
+
+#remove key-value format
+def remove_kv(key_value):
+    date_time = key_value[0]
+    for price in key_value[1]:
+         date_time = date_time + "," + price
+    return date_time
+unrolled = no_bus_names.map(remove_kv) 
+unrolled.saveAsTextFile("/home/kenlee/energy_market_project/LMP_data/DAM_Hr_LMP_2010/formatted_merge_test");
 
 
 
