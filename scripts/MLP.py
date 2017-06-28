b2210aec9e0a65d3a08ec73fa5a1a5ad860195c2
kenleejr92 <kensoccer@gmail.com>
2017-06-28 14:39:03 -0500

    MLP code added

--- scripts/MLP.py
+++ scripts/MLP.py
@@ -1,13 +1,39 @@
-mport sys
-sys.path.append('..')
-import numpy as np
-from skimage import io
-import tensorflow as tf
+from sklearn.neural_network import MLPRegressor
+#x is your training series as a matrix
+#t is your testing series as a matrix
+#Training
+X = []
+y = []
+for i in np.arange(25, x.shape[0]):
+    lags = []
+    for k in np.arange(1, 25):
+        lags.append(x[i-k])
+    X.append(lags)
+    y.append(x[i])
+X = np.squeeze(np.array(X))
+y = np.array(y)
 
 
-class MLP(object):
+#Change parameters to MLP
+MLP = MLPRegressor()
+MLP.fit(X, y)
 
-	def __init__():
-		pass
+#Testing
+X = []
+y = []
+for i in np.arange(25, t.shape[0]):
+    lags = []
+    for k in np.arange(1, 25):
+        lags.append(t[i-k])
+    X.append(lags)
+    y.append(t[i])
+X = np.squeeze(np.array(X))
+y = np.array(y)
 
-	
+y_pred = MLP.predict(X)
+
+print np.mean(np.abs(y_pred-y))
+plt.plot(y_pred, label='predicted')
+plt.plot(y, label='actual')
+plt.legend()
+plt.show()
